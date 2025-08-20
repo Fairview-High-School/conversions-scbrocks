b54edee6b0301d8ddc216bfb49fb48d51ee7d07f
scbrocks <scbrown01@bvsd.org>
2025-08-20 18:56:45 +0000

    Added menu

--- Conversions.java
+++ Conversions.java
@@ -1,1 +1,15 @@
+// Steven C. Brown
+// 08/20/2025
 
+public class Conversions
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1. Fahrenheit to Celcius");
+        System.out.println("2. Celcius to Fahrenheit");
+        System.err.println("3. Feet to Meters");
+        System.out.println("4. Meters to Feet");
+        System.out.println("5. Ounces to Milliliters");
+        System.out.println("6. Milliliters to Ounces");
+    }
+}
